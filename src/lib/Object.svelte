be94d3de532176877b7ef79ffae13b9a7fdd79f8
Doug A <douganderson444@gmail.com>
2022-03-16 13:42:48 -0300

    disp object

---
+++ src/lib/Object.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let val;
+</script>
+
+{#if val}
+	<ul>
+		{#each [...Object.entries(val)] as [key, val]}
+			<li>
+				{#if typeof val === 'object'}
+					{key} <svelte:self {key} {val} />
+				{:else}
+					{key}
+					:
+					{val}
+				{/if}
+			</li>
+		{/each}
+	</ul>
+{/if}
+
+<style>
+	ul {
+		list-style-type: square;
+	}
+</style>
