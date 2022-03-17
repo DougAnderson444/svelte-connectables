67252f8713d0999d70b78ab954d2b115219d18ab
Doug A <douganderson444@gmail.com>
2022-03-17 14:28:02 -0300

    export

---
+++ src/lib/index.js
@@ -0,0 +1,9 @@
+import Connectable from './Connectable.svelte';
+import Links from './Links.svelte';
+import PanHandle from './PanHandle.svelte';
+import Resizable from './Resizable.svelte';
+
+export { Connectable };
+export { Links };
+export { PanHandle };
+export { Resizable };
