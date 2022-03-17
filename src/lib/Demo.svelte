bc49e0ac4037d500e98aa0a9cd5067990552bace
Doug A <douganderson444@gmail.com>
2022-03-17 13:28:15 -0300

    add Resizable

--- src/lib/Demo.svelte
+++ src/lib/Demo.svelte
@@ -4,6 +4,7 @@
 	import PanHandle from './PanHandle.svelte';
 	import Links from './Links.svelte';
 	import Object from './Object.svelte';
+	import Resizable from './Resizable.svelte';
 
 	let nodes = {
 		connections: []
@@ -14,22 +15,23 @@
 
 	let data = {
 		nodes: [
-			{ id: '1', group: 1, connectable: true, x: 100, y: 100 },
-			{ id: '2', group: 2, connectable: false, x: 414, y: 190 },
-			{ id: '3', group: 2, connectable: false, x: 250, y: 300 }
+			{ id: '1', group: 1, connectable: true, x: 100, y: 100, width: 100, height: 100 },
+			{ id: '2', group: 2, connectable: false, x: 414, y: 190, width: 100, height: 100 },
+			{ id: '3', group: 2, connectable: false, x: 250, y: 300, width: 100, height: 100 }
 		],
 		links: [{ source: { id: '1' }, target: { id: '2' } }]
 	};
 </script>
 
 <!-- Troubleshooting data  -->
-<!-- <Object val={data} /> -->
 
 <div class="wrapper">
 	{#each data.nodes as node (node.id)}
 		<Connectable bind:data bind:node>
-			<PanHandle {nodes} bind:node />
-			{node.id} Connect from Me
+			<Resizable bind:node>
+				<PanHandle bind:node />
+				{node.id} <span>Connect from Me</span>
+			</Resizable>
 		</Connectable>
 	{/each}
 
@@ -37,11 +39,15 @@
 		<Links {data} />
 	{/if}
 </div>
+<div>
+	<Object val={data} />
+</div>
 
 <style>
 	.wrapper {
 		display: flex;
 		position: relative;
+		left: 0px;
 		margin: 0px;
 		border: dotted 3px black;
 		border-radius: 4px;
